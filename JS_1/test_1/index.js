4d913c0c4dbb91d449458352efa677b65b82ebfa
lahiruharshana <lharshana2002@gmail.com>
2023-09-27 20:44:52 +0530

    updated

--- JS_1/test_1/index.js
+++ JS_1/test_1/index.js
@@ -53,4 +53,5 @@ let userName;
 document.getElementById("myButton").onclick = function (){
     userName=document.getElementById("myText").value;
     console.log(userName);
+    document.getElementById("mylabel").innerHTML = userName;
 }
